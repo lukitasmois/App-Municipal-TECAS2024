9d4f0cfda4806895278483873c449f0c4cb0782b
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-14 01:43:41 -0300

    Controlador y ruta para ver un negocio

--- backend/routes/negocio.js
+++ backend/routes/negocio.js
@@ -2,20 +2,24 @@ const express = require("express");
 const router = express.Router();
 const validarNegocio = require("../validaciones/validarNegocio");
 const { catchAsync } = require("../utils");
-const crearNegocio = require("../controllers/negocio")
+const { crearNegocio, VerNegocio } = require("../controllers/negocio");
 
 //configuracion para multer (guardar archivos)
-const multer = require('multer')
-const subirArchivo = multer({dest: 'archivos/'}) 
+const multer = require("multer");
+const subirArchivo = multer({ dest: "archivos/" });
 
 //Ruta para crear el negocio
 // Middleware para subir múltiples archivos (máximo 2) con el campo 'archivos'
 // Middleware para validar los datos del negocio en el cuerpo de la solicitud
 /*Middleware para manejar errores, si no hay ningun error
 se carga el negocio con todos los */
-router.post("/crearNegocio", 
-    subirArchivo.array('archivos', 2),
-    validarNegocio
-    ,catchAsync(crearNegocio))
+router.post(
+  "/crearNegocio",
+  subirArchivo.array("archivos", 2),
+  validarNegocio,
+  catchAsync(crearNegocio)
+);
 
-module.exports = router
+router.get("/vernegocio", VerNegocio);
+
+module.exports = router;
---
+++ frontend/src/views/negocio/VerNegocio.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+function VerNegocio() {
+  return <div></div>;
+}
+
+export default VerNegocio;

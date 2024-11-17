a987b98f6bedfb6cf0b19a0cf9eb93e731b15e92
Maximiliano Agustín Especie Calcagno <56851501+maximilianocalcagno@users.noreply.github.com>
2024-11-17 04:32:10 -0300

    Barra de navegación hecha
    Falta boton de cerrar sesion y estilos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Inicio from "./views/Inicio.jsx";
 import EditarUsuario from "./views/usuario/EditarUsuario.jsx";
 import { useAuth } from "./useAuth.jsx";
 import CrearNegocioScreen from "./views/negocio/CrearNegocioScreen.jsx";
 import { ToastContainer } from "react-toastify";
-
 import  ProtectedRoute  from "./components/InicioProtegido.jsx";
+import BarraNavegacion from "./components/BarraNavegacion.jsx";
+
 function App() {
 
   const { usuarioLogeado, setUsuarioLogeado, fetchUsuarioLogeado } = useAuth();
 
   return (
     <>
+      <BarraNavegacion usuarioLogeado={usuarioLogeado}></BarraNavegacion>
       <Routes>
         <Route path="/" element={<Inicio usuarioLogeado={usuarioLogeado} />} />
 
---
+++ frontend/src/components/BarraNavegacion.jsx
@@ -0,0 +1,60 @@
+import { NavLink } from "react-router-dom";
+
+function BarraNavegacion({usuarioLogeado}) 
+{
+    function navegacionDeslogeado()
+    {
+        if (!usuarioLogeado.logeado){
+            return (
+            <>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/">Inicio</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/Preguntas-frecuentes">Preguntas frecuentes</NavLink>
+                </li>
+            </>
+            )
+        }
+    }
+
+    function navegacionLogeado()
+    {
+        if(usuarioLogeado.logeado){
+            return (
+                <>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/">Inicio</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/Preguntas-frecuentes">Preguntas frecuentes</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/">Cerrar sesion</NavLink>
+                    </li>
+                </>
+            )
+        }
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        {navegacionDeslogeado()}
+                    </ul>
+                    <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+                        {navegacionLogeado()}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+)
+}
+
+export default BarraNavegacion;

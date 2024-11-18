941f57ad4b4e5c4706acbd9f78e11e23d9cf2ce5
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-18 20:12:57 -0300

    Resolucion de conflictos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Inicio from "./views/Inicio.jsx";
 import EditarUsuario from "./views/usuario/EditarUsuario.jsx";
 import { useAuthContext } from "./context/AuthContext.jsx";
@@ -8,9 +8,9 @@ import CrearNegocioScreen from "./views/negocio/CrearNegocioScreen.jsx";
 import VerNegocio from "./views/negocio/VerNegocio.jsx";
 import { ToastContainer } from "react-toastify";
 import GetBussinesesData from "./components/DatosDeNegocio.jsx";
-import ProtectedRoute  from "./components/InicioProtegido.jsx";
+import ProtectedRoute from "./components/InicioProtegido.jsx";
 import BarraNavegacion from "./components/BarraNavegacion.jsx";
-import VistaHabilitaciones from "./views/habilitaciones/VistaHabilitaciones.jsx";
+import VistaHabilitaciones from "./views/habilitacion/Habilitaciones.jsx";
 import CerrarSesion from "./views/usuario/CerrarSesion.jsx";
 import { Toaster } from "react-hot-toast";
 import { VerPlanos } from "./views/administracion/verPlanos.jsx";
@@ -22,12 +22,20 @@ function App() {
       <BarraNavegacion></BarraNavegacion>
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/administracion/ver-planos" element={<RoleProtectedRoute allowedRoles={["EMPLEADO"]} nextView={<VerPlanos/>}/>}/>
-        <Route path="/ver_negocios/:id" element={<GetBussinesesData />}/>
+        <Route
+          path="/administracion/ver-planos"
+          element={
+            <RoleProtectedRoute
+              allowedRoles={["EMPLEADO"]}
+              nextView={<VerPlanos />}
+            />
+          }
+        />
+        <Route path="/ver_negocios/:id" element={<GetBussinesesData />} />
         <Route path="/cerrar-sesion" element={<CerrarSesion />}></Route>
         <Route path="/editar-usuario/:id" element={<EditarUsuario />} />
         <Route path="/crearNegocio" element={<CrearNegocioScreen />} />
-        <Route path="/habilitaciones" element={<VistaHabilitaciones />}/>
+        <Route path="/habilitaciones" element={<VistaHabilitaciones />} />
         <Route path="/vernegocio/:idNegocio" element={<VerNegocio />} />
       </Routes>
       <ToastContainer />
--- frontend/src/components/BarraNavegacion.jsx
+++ frontend/src/components/BarraNavegacion.jsx
@@ -3,50 +3,67 @@ import { useAuthContext } from "../context/AuthContext";
 import "./BarraNavegacion.css";
 import logoMunicipalidad from "../assets/logo-municipio.png";
 
-function BarraNavegacion() 
-{
+function BarraNavegacion() {
+  const { usuarioLogeado } = useAuthContext();
 
-    const { usuarioLogeado } = useAuthContext();
-
-    function navegacionLogeado()
-    {
-        if(usuarioLogeado.logeado){
-            return (
-                <>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/" >Hola {usuarioLogeado.usuario.nombre}!</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/cerrar-sesion">Cerrar sesion</NavLink>
-                    </li>
-                </>
-            )
-        }
+  function navegacionLogeado() {
+    if (usuarioLogeado.logeado) {
+      return (
+        <>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Hola {usuarioLogeado.usuario.nombre}!
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/cerrar-sesion">
+              Cerrar sesion
+            </NavLink>
+          </li>
+        </>
+      );
     }
+  }
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div class="container-fluid">
-            <img src={logoMunicipalidad} style={{ maxWidth: "180px", maxHeight: "48px" }} alt="Logo Municipalidad" />
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Inicio</NavLink>
-                            </li>
-                            <li className="nav-item">
-                            <NavLink className="nav-link" to="/Preguntas-frecuentes">Preguntas frecuentes</NavLink>
-                            </li>
-                        </ul>
-                        <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-                            {navegacionLogeado()}
-                        </ul>
-                </div>
-            </div>
-        </nav>
-)
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <img
+          src={logoMunicipalidad}
+          style={{ maxWidth: "180px", maxHeight: "48px" }}
+          alt="Logo Municipalidad"
+        />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Preguntas-frecuentes">
+                Preguntas frecuentes
+              </NavLink>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+            {navegacionLogeado()}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default BarraNavegacion;

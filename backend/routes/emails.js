2150b7bb55e33d6ee86f58c20647209abae6cbc4
lukitasmois <lmoix85@gmail.com>
2024-11-23 10:36:23 -0300

    Modularizacion de la funcion para enviar un mail

--- backend/routes/emails.js
+++ backend/routes/emails.js
@@ -1,19 +1,6 @@
 const express = require("express");
-const emailTemplates = require("../emails/templates")
-const {emailQueue} = require("../emails/connection.js")
+const sendMail = require("../controllers/email");
 const emailsRouter = express.Router();
 
-
-emailsRouter.post("/sendEmail", async (req, res) => {
-    try {
-        const { to, subject, template, params } = req.body;
-        const html = emailTemplates[template](params);
-        const job = { to, subject, html };
-        await emailQueue.add(job);
-        res.status(200).send("Email en la cola para ser enviado");
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-});
-
+emailsRouter.post("/sendEmail", sendMail)
 module.exports = emailsRouter

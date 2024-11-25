0c6fc959f94dee8164215bb13003b74f9379fe28
Maximiliano Agustín Especie Calcagno <56851501+maximilianocalcagno@users.noreply.github.com>
2024-11-25 01:03:31 -0300

    Agregados estilos y el boton para ver negocios
    El boton tiene la funcionalidad para redirigirte a la vista de los negocios

--- frontend/src/views/usuario/VerUsuario.jsx
+++ frontend/src/views/usuario/VerUsuario.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Contenedor from "../../components/Contenedor";
 
 function VerUsuario() {
@@ -23,15 +23,21 @@ function VerUsuario() {
 
     return (
         <div>
-            <Contenedor>
+            
             <h1>Mi perfil</h1>
-            <img src={usuario.imagen} alt="Imagen de perfil" />
+            <div className="w-50 m-auto">
+            <Contenedor>
+            <p>Imagen de perfil: <img className="rounded-circle" style={{ width: "50px", height: "50px", border: "1px solid grey" }} src={usuario.imagen} alt="Imagen de perfil" /> </p>
             <p>Nombre: {usuario.nombre}</p>
             <p>Apellido: {usuario.apellido}</p>
             <p>CUIT: {usuario.cuil}</p>
             <p>Telefono: {usuario.telefono}</p>
             <p>Email: {usuario.email}</p>
+            <p>Rol: {usuario.rol}</p>
+
+            <Link to={`/ver_negocios/${id}`} btn className="btn btn-primary">Ver negocios</Link>
             </Contenedor>
+            </div>
         </div>
     );
 }
